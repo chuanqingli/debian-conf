8a20db4f3a49c77abd45cbdb5ec768ea211a89dc
chuanqingli <9915252@qq.com>
2018-09-29 17:30:45 +0800

    新建

---
+++ debian-install.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+<<'COMMENT'
+知识点：
+1、archlinux安装脚本；
+2、echo的增强输出；
+3、curl、wget下载文件，输出格式化；
+4、mktemp指定临时文件；
+5、字符串分组及匹配；
+6、数组和文本的两种遍历（index、value）；
+7、sed格式化或清除指定内容；
+8、grep查找内容；
+9、变量的计算；
+10、下标数组和关联数组；
+COMMENT
+
+install-soft(){
+    pkgs=(
+
+        # 输入法
+	    fcitx fcitx-table-wbpy fcitx-config-gtk
+        # 编辑器
+        vim-gtk emacs25 eclipse
+        # ftp
+        filezilla
+        # 版本控制
+        git subversion
+        # 邮箱
+        thunderbird
+        zsh curl
+        # 远程服务器
+        xrdp vnc4server
+        # 远程桌面
+        rdesktop
+        openssh-server
+    )
+
+    apt-get install ${pkgs[*]}
+}
+
+config-ssh(){
+    checkok=`grep "^[ ]*PasswordAuthentication yes" /etc/ssh/ssh_config`
+    if [[ ! ${checkok} =~ "PasswordAuthentication" ]];then
+        echo "PasswordAuthentication yes">>/etc/ssh/ssh_config
+    fi
+
+    checkok=`grep "^[ ]*PermitRootLogin yes" /etc/ssh/sshd_config`
+    if [[ ! ${checkok} =~ "PermitRootLogin" ]];then
+        echo "PermitRootLogin yes">>/etc/ssh/sshd_config
+    fi
+
+    systemctl enable ssh
+    systemctl enable sshd
+    systemctl restart ssh
+    systemctl restart sshd
+}
+
+config-zsh(){
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo "ZSH_THEME plugins 节点多的话，请自行删除。两节点要加在source \$ZSH/oh-my-zsh.sh之前。"
+
+    sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="ys"/g' ~/.zshrc
+    # 跨行替换
+    sed -i ":begin; /^plugins=(/,/)/ { /)/! { $! { N; b begin }; }; s/plugins=(.*)/plugins=(git autojump zsh-autosuggestions)/; };" ~/.zshrc
+}
+
+config-chrome-lastpass(){
+    srcdir=/home/bak/soft/zip
+    fname=lpchrome_linux
+    dstdir=/opt/google/chrome/${fname}
+    # 没下载过就要下载
+    if [[ ! -e ${srcdir}/${fname}.zip ]];then
+        wget https://lastpass.com/${fname}.crx -O ${srcdir}/${fname}.zip
+    fi
+    
+    mkdir -p ${dstdir}
+    cp ${srcdir}/${fname}.zip ${dstdir}
+    unzip ${dstdir}/${fname}.zip -d ${dstdir}
+    rm ${dstdir}/${fname}.zip
+}
+
+config-wps-font(){
+    srcdir=/home/bak/soft/zip
+    dstdir=/usr/share/fonts
+    fname=wps_symbol_fonts
+
+    cp ${srcdir}/${fname}.zip ${dstdir}
+    unzip ${dstdir}/${fname}.zip -d ${dstdir}
+    rm ${dstdir}/${fname}.zip
+    rundir=`pwd`
+    cd ${dstdir}
+    # 执行以下命令,生成字体的索引信息：
+    mkfontscale && mkfontdir && fc-cache
+    cd ${rundir}
+}
+
+config-jdk6(){
+    srcdir=/home/bak/soft/bin
+    dstdir=/opt/java
+    fname=jdk-6u45-linux-x64
+
+    mkdir -p ${dstdir}
+    cp ${srcdir}/${fname}.bin ${dstdir}
+
+    rundir=`pwd`
+    cd ${dstdir}
+    bash ./${fname}.bin
+    rm ${fname}.bin
+    cd ${rundir}
+
+    for ename in "java" "javac" "jar";do
+        update-alternatives --install /usr/bin/${ename} ${ename} /opt/java/jdk1.6.0_45/bin/${ename} 300
+    done
+
+
+    checkok=`grep "^JAVA_HOME" /etc/profile`
+    if [[ ${checkok} =~ "JAVA_HOME" ]];then
+        return
+    fi
+
+    #echo "export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))">>/etc/profile
+    echo "export JAVA_HOME=$(dirname $(dirname $(update-alternatives --list java|head -n 1)))">>/etc/profile
+    echo "export JRE_HOME=$JAVA_HOME/jre">>/etc/profile
+    echo "export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib">>/etc/profile
+    echo "export PATH=$JAVA_HOME/bin:$PATH">>/etc/profile
+}
+
+
+$1
